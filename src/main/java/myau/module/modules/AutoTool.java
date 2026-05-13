05dd33db764f280aa4502d9dccfb72d53c541cf4
7fxloloyousef-arch <7fxloloyousef@gmail.com>
2026-05-13 17:41:13 +0100

    Update AutoTool.java

--- src/main/java/myau/module/modules/AutoTool.java
+++ src/main/java/myau/module/modules/AutoTool.java
@@ -18,9 +18,12 @@ public class AutoTool extends Module {
     private int currentToolSlot = -1;
     private int previousSlot = -1;
     private int tickDelayCounter = 0;
+
     public final IntProperty switchDelay = new IntProperty("delay", 0, 0, 5);
     public final BooleanProperty switchBack = new BooleanProperty("switch-back", true);
     public final BooleanProperty sneakOnly = new BooleanProperty("sneak-only", true);
+    public final BooleanProperty instantSwitch = new BooleanProperty("instant-switch", true);
+    public final BooleanProperty instantSwitchBack = new BooleanProperty("instant-switch-back", true);
 
     public AutoTool() {
         super("AutoTool", false);
@@ -32,46 +35,94 @@ public class AutoTool extends Module {
         return TeamUtil.isEntityLoaded(killAura.getTarget()) && killAura.isAttackAllowed();
     }
 
+    /**
+     * Switch to the best tool for the block being looked at.
+     * Returns true if a switch happened.
+     */
+    private boolean switchToTool() {
+        int slot = ItemUtil.findInventorySlot(
+                mc.thePlayer.inventory.currentItem,
+                mc.theWorld.getBlockState(mc.objectMouseOver.getBlockPos()).getBlock()
+        );
+
+        if (mc.thePlayer.inventory.currentItem != slot) {
+            if (this.previousSlot == -1) {
+                this.previousSlot = mc.thePlayer.inventory.currentItem;
+            }
+            mc.thePlayer.inventory.currentItem = this.currentToolSlot = slot;
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Switch back to the previous slot immediately.
+     */
+    private void switchBack() {
+        if (this.previousSlot != -1) {
+            mc.thePlayer.inventory.currentItem = this.previousSlot;
+        }
+        resetState();
+    }
+
+    private void resetState() {
+        this.currentToolSlot = -1;
+        this.previousSlot = -1;
+        this.tickDelayCounter = 0;
+    }
+
     @EventTarget
     public void onTick(TickEvent event) {
-        if (this.isEnabled() && event.getType() == EventType.PRE) {
-            if (this.currentToolSlot != -1 && this.currentToolSlot != mc.thePlayer.inventory.currentItem) {
-                this.currentToolSlot = -1;
-                this.previousSlot = -1;
-            }
-            if (mc.objectMouseOver != null
-                    && mc.objectMouseOver.typeOfHit == MovingObjectType.BLOCK
-                    && mc.gameSettings.keyBindAttack.isKeyDown()
-                    && !mc.thePlayer.isUsingItem()
-                    && !isKillAura()) {
-                if (this.tickDelayCounter >= this.switchDelay.getValue()
-                        && (!(Boolean) this.sneakOnly.getValue() || KeyBindUtil.isKeyDown(mc.gameSettings.keyBindSneak.getKeyCode()))) {
-                    int slot = ItemUtil.findInventorySlot(
-                            mc.thePlayer.inventory.currentItem, mc.theWorld.getBlockState(mc.objectMouseOver.getBlockPos()).getBlock()
-                    );
-                    if (mc.thePlayer.inventory.currentItem != slot) {
-                        if (this.previousSlot == -1) {
-                            this.previousSlot = mc.thePlayer.inventory.currentItem;
-                        }
-                        mc.thePlayer.inventory.currentItem = this.currentToolSlot = slot;
-                    }
+        if (!this.isEnabled() || event.getType() != EventType.PRE) return;
+
+        // If player manually changed slot, cancel the auto tool
+        if (this.currentToolSlot != -1 && this.currentToolSlot != mc.thePlayer.inventory.currentItem) {
+            resetState();
+            return;
+        }
+
+        boolean lookingAtBlock = mc.objectMouseOver != null
+                && mc.objectMouseOver.typeOfHit == MovingObjectType.BLOCK;
+        boolean attacking = mc.gameSettings.keyBindAttack.isKeyDown()
+                && !mc.thePlayer.isUsingItem()
+                && !isKillAura();
+        boolean sneakOk = !(Boolean) this.sneakOnly.getValue()
+                || KeyBindUtil.isKeyDown(mc.gameSettings.keyBindSneak.getKeyCode());
+
+        if (lookingAtBlock && attacking && sneakOk) {
+            // Instant switch - bypass the delay entirely on first hit
+            if (instantSwitch.getValue()) {
+                switchToTool();
+            } else {
+                // Normal delay-based switch
+                if (this.tickDelayCounter >= this.switchDelay.getValue()) {
+                    switchToTool();
                 }
                 this.tickDelayCounter++;
-            } else {
-                if (this.switchBack.getValue() && this.previousSlot != -1) {
+            }
+        } else {
+            // Not attacking or not looking at block anymore
+            if (this.switchBack.getValue() && this.previousSlot != -1) {
+                if (instantSwitchBack.getValue()) {
+                    // Switch back immediately same tick
+                    switchBack();
+                } else {
+                    // Original behaviour - switch back next tick
                     mc.thePlayer.inventory.currentItem = this.previousSlot;
+                    resetState();
                 }
-                this.currentToolSlot = -1;
-                this.previousSlot = -1;
-                this.tickDelayCounter = 0;
+            } else {
+                resetState();
             }
         }
     }
 
     @Override
     public void onDisabled() {
-        this.currentToolSlot = -1;
-        this.previousSlot = -1;
-        this.tickDelayCounter = 0;
+        // Switch back to previous slot before disabling
+        if (this.switchBack.getValue() && this.previousSlot != -1) {
+            mc.thePlayer.inventory.currentItem = this.previousSlot;
+        }
+        resetState();
     }
 }
